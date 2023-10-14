ba5e5b8b335f17d28cfda9f3fd64a7dccad2b80a
Hasin Hayder <countdraculla@gmail.com>
2023-10-14 12:39:49 +0600

    Load more posts

--- src/components/Blog.vue
+++ src/components/Blog.vue
@@ -3,6 +3,8 @@ import { ref, onBeforeMount } from 'vue'
 import { useRoute } from 'vue-router'
 const route = useRoute()
 
+const skip = ref(20)
+
 const posts = ref([])
 onBeforeMount(() => {
     fetch('https://dummyjson.com/posts?limit=20')
@@ -11,6 +13,15 @@ onBeforeMount(() => {
             posts.value = data.posts
         })
 })
+
+function loadMorePosts(){
+    fetch(`https://dummyjson.com/posts?limit=20&skip=${skip.value}`)
+        .then(res => res.json())
+        .then(data => {
+            posts.value = [...posts.value, ...data.posts]
+            skip.value += 20
+        })
+}
 </script>
  
 <template>
@@ -28,6 +39,11 @@ onBeforeMount(() => {
             {{ post.body }}
         </p>
     </article>
+    <div class="my-5">
+        <button @click="loadMorePosts()" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          Load More
+        </button>
+    </div>
 </template>
  
 <style scoped></style>
